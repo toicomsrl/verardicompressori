4c0a99d648602281d1b0601b1c15c2f098483ae8
LucaAntoniolli <lucaanto1981@gmail.com>
2024-06-10 13:57:22 +0200

    Prosecuzione pagina assistenza

--- page-assistenza-compressori-industriali.php
+++ page-assistenza-compressori-industriali.php
@@ -7,6 +7,7 @@ $primoTesto = get_field('assistenza_primo_testo');
 $secondoSottotitolo = get_field('assistenza_secondo_sottotitolo');
 $secondoTesto = get_field('assistenza_secondo_testo');
 $scrittaCheScorre = get_field('assistenza_scritta_che_scorre');
+$slides = get_field('assistenza_slides');
 ?>
 
 <div class="pbmit-title-bar-wrapper" style="background-image: url('<?php echo $immagine; ?>'">
@@ -19,119 +20,57 @@ $scrittaCheScorre = get_field('assistenza_scritta_che_scorre');
     </div>
 </div>
 
-<!-- SLIDE CHE SI SOVRAPPONGONO -->
-<section class="section-xl" data-cursor="global-color" style="background-color: #979797;">
-    <div class="container" style="background-color: #979797;">
-        <div class=" pbmit-element-card-box-style-1">
-            <div class="pbmit-content-box">
-                <div class="pbmit-card-box-wrapper">
-                    <div class="pbmit-card-box-wrapper-inner d-flex align-items-center justify-content-between">
-                        <div class="pbmit-card-box-img col-lg-6 col-md-12">
-                            <div class="pbmit-card-box-item-image">
-                                <img src="images/homepage-01/card-img-01.jpg" alt="Answer a Few Questions">
-                            </div>
-                        </div>
-                        <div class="pbmit-card-box col-lg-6 col-md-12">
-                            <div class="pbmit-card-box-number">
-                                <h4>01</h4>
-                            </div>
-                            <div class="pbmit-card-box-title">
-                                <h4>Answer a Few Questions</h4>
-                            </div>
-                            <div class="pbmit-card-box-desc">We welcome and celebrate different perspectives to help our firm, our clients and our people achieve enduring results.</div>
-                            <div class="pbmit-card-box-line">
-                                <div class="pbmit-card-box-line-inner">
-                                    <p>Translating the design into code using Webflow</p>
-                                </div>
-                                <div class="pbmit-card-box-line-inner">
-                                    <p>Responsive and functions perfectly across all devices</p>
-                                </div>
-                                <div class="pbmit-card-box-line-inner">
-                                    <p>Testing the website thoroughly before launch</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="pbmit-card-box-wrapper">
-                    <div class="pbmit-card-box-wrapper-inner d-flex align-items-center justify-content-between">
-                        <div class="pbmit-card-box-img col-lg-6 col-md-12">
-                            <div class="pbmit-card-box-item-image">
-                                <img src="images/homepage-01/card-img-02.jpg" alt="Changing of finance goals">
-                            </div>
-                        </div>
-                        <div class="pbmit-card-box col-lg-6 col-md-12">
-                            <div class="pbmit-card-box-number">
-                                <h4>02</h4>
-                            </div>
-                            <div class="pbmit-card-box-title">
-                                <h4>Changing of finance goals</h4>
-                            </div>
-                            <div class="pbmit-card-box-desc">Interview vetted advisors to find the best fit Lorem ipsum dolor sit amet, consectetur adip iscing elit.</div>
-                            <div class="pbmit-card-box-line">
-                                <div class="pbmit-card-box-line-inner">
-                                    <p>Translating the design into code using Webflow</p>
-                                </div>
-                                <div class="pbmit-card-box-line-inner">
-                                    <p>Responsive and functions perfectly across all devices</p>
-                                </div>
-                                <div class="pbmit-card-box-line-inner">
-                                    <p>Testing the website thoroughly before launch</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="pbmit-card-box-wrapper">
-                    <div class="pbmit-card-box-wrapper-inner d-flex align-items-center justify-content-between">
-                        <div class="pbmit-card-box-img col-lg-6 col-md-12">
-                            <div class="pbmit-card-box-item-image">
-                                <img src="images/homepage-01/card-img-03.jpg" alt="Increase Your Net Worth">
-                            </div>
-                        </div>
-                        <div class="pbmit-card-box col-lg-6 col-md-12">
-                            <div class="pbmit-card-box-number">
-                                <h4>03</h4>
-                            </div>
-                            <div class="pbmit-card-box-title">
-                                <h4>Increase Your Net Worth</h4>
-                            </div>
-                            <div class="pbmit-card-box-desc">Interview vetted advisors to find the best fit Lorem ipsum dolor sit amet, consectetur adip iscing elit.</div>
-                            <div class="pbmit-card-box-line">
-                                <div class="pbmit-card-box-line-inner">
-                                    <p>Translating the design into code using Webflow</p>
-                                </div>
-                                <div class="pbmit-card-box-line-inner">
-                                    <p>Responsive and functions perfectly across all devices</p>
-                                </div>
-                                <div class="pbmit-card-box-line-inner">
-                                    <p>Testing the website thoroughly before launch</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
+<section>
+    <div class="container-bianco-testo">
+        <div class="container">
+            <div class="row testo">
+                <div class="col-12">
+                    <h2><?php echo $primoSottotitolo; ?></h2>
+                    <p><?php echo $primoTesto ?></p>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!-- FINE SLIDE CHE SI SOVRAPPONGONO -->
 
-<div class="container-bianco-testo">
-    <div class="container">
-        <div class="row testo">
-            <div class="col-12">
-                <h2><?php echo $primoSottotitolo; ?></h2>
-                <p><?php echo $primoTesto ?></p>
+<!-- SLIDE CHE SI SOVRAPPONGONO -->
+<?php if (!empty($slides)) { ?>
+
+    <section class="section-xl" data-cursor="global-color" style="background-color: #323232;">
+        <div class="container" style="background-color: #323232;">
+            <div class=" pbmit-element-card-box-style-1">
+                <div class="pbmit-content-box">
+                    <?php foreach ($slides as $slide) { ?>
+                        <div class="pbmit-card-box-wrapper">
+                            <div class="pbmit-card-box-wrapper-inner d-flex align-items-center justify-content-between">
+                                <div class="pbmit-card-box-img col-lg-6 col-md-12">
+                                    <div class="pbmit-card-box-item-image">
+                                        <img src="<?php echo $slide['immagine_slide']; ?>" alt="Answer a Few Questions">
+                                    </div>
+                                </div>
+                                <div class="pbmit-card-box col-lg-6 col-md-12">
+                                    <div class="pbmit-card-box-title">
+                                        <h4><?php echo $slide['titolo_slide']; ?></h4>
+                                    </div>
+                                    <hr class="pbmit-card-box-divider" style="margin-block: 30px; color: #979797">
+                                    <div class=" pbmit-card-box-desc"><?php echo $slide['sottotitolo_slide']; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
+<?php } ?>
+<!-- FINE SLIDE CHE SI SOVRAPPONGONO -->
 
-<div class="container-bianco-testo">
+<!-- INIZIO BLOCCO CON ICONE -->
+
+<!-- FINE BLOCK CON ICONE -->
+
+<section>
     <div class="container">
         <div class="row testo">
             <div class="col-12">
@@ -140,10 +79,21 @@ $scrittaCheScorre = get_field('assistenza_scritta_che_scorre');
             </div>
         </div>
     </div>
-</div>
+</section>
 
+<section>
+    <div class="container-grigio-testo">
+        <div class=" container">
+            <div class="row testo">
+                <div class="col-12">
+                    <?php the_content() ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
-<!-- SCRITTA CHE CORRE -->
+<!-- SCRITTA CHE SCORRE -->
 
 <section class="section-lgb pbminfotech-element-marquee-effect">
     <div class="container-fluid p-0">
